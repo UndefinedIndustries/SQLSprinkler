6889dd99db38496c95bba3ab7f890f585c4e9999
GT3CH1 <gavinpease@gmail.com>
2017-04-10 12:44:46 -0600

    This thing actually kinda works

--- lib/check.php
+++ lib/check.php
@@ -1,22 +1,23 @@
 <br>
 <br><?php
 	$a = '';
-    $servername = "localhost"; //server ip for sql
+    $servername = "192.168.1.181"; //server ip for sql
     $username   = "root"; //server username
     $password   = "#FiddleFire"; //server password
-    $dbname     = "Computers"; //database name
+    $dbname     = "SQLSprinkler"; //database name
     $conn       = mysqli_connect($servername, $username, $password, $dbname); //connect to sql database with all parameters
 
-    $sql    = "SELECT * FROM `list`"; // select only the username field from the table "users_table"
+    $sql    = "SELECT * FROM `Systems`"; // select only the username field from the table "users_table"
     $result = mysqli_query($conn, $sql); // process the query
     $username_array = array(); // start an array
     while ($row = mysqli_fetch_array($result)) // cycle through each record returned
       {
+	$a = 0;
         $sysname[] = "\"" . $row['Name'] . "\""; // get the username field and add to the array above with surrounding quotes
         $sysgpio[]       = "\"" . $row['GPIO'] . "\""; // get the ip field and add to the array
-        $name             = trim($username_array[$a], '"'); //trim " " from username
+        $name             = trim($sysname[$a], '"'); //trim " " from username
         $gpio               = trim($ip_array[$a], '"'); //trim " " from ip
-
+	$a++;
 	    exec ( "gpio read -g".$gpio."", $status );
     
 	foreach ($status as $value){
