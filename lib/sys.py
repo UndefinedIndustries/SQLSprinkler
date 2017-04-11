2564a61e39c9af90f5f56edcf4ed15de34af2632
GT3CH1 <gavinpease@gmail.com>
2017-04-10 20:54:36 -0600

    Forgot the correct sys.py file -.-

--- lib/sys.py
+++ lib/sys.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
-dir = "/var/www/html/data/sys"
-import RPi.GPIO as GPIO
+import MySQLdb
 import time
-a = 0 
-GPIO.setmode(GPIO.BCM)
-pin = [13,18,23,17,27,22,10,9,11,19]
-for i in pin:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, False)
-    a = a + 1
-    data = open(dir +str(a)+".dat");
-    test = data.read();
-    time.sleep(float(test));
-    GPIO.output(i, True)
-quit()
+import RPi.GPIO as GPIO
+db = MySQLdb.connect(host="192.168.1.181",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="#FiddleFire",  # your password
+                     db="SQLSprinkler")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT gpio, Time FROM Systems ")
+
+# print all the first cell of all the rows
+for row in list(cur.fetchall()):
+	GPIO.setmode(GPIO.BCM)
+	print row[0]
+        GPIO.setup(int(row[0]), GPIO.OUT)
+	GPIO.output(int(row[0]), False)
+	print row[1]*60
+	time.sleep(int(row[1])*60)
+	GPIO.output(int(row[0]), True)
+db.close()
