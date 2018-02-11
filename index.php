2a38028f6eda31ebb8c00e5606bbcdbc6cfc73d0
GT3CH1 <gavinpease@gmail.com>
2018-02-11 10:26:15 -0700

    Fixed things again

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
             
                         }
  ?>
+SAMPLE TEXT FOR SAMPLE
 <!DOCTYPE html>
  <html>
    <head>
--- lib/updater.php
+++ lib/updater.php
@@ -8,7 +8,7 @@
  ?> 
 <?php 
  exec('git -C ../ fetch --all & git reset --hard origin/master & git pull origin master & git -C ../ fetch --all & git reset --hard origin/master & git pull origin master');
- exec('sudo chmod 777 -R ../sys.dat');
+ exec('sudo chmod 777 -R ../');
  header('Location: ../');
  ?>
  
