1c44352c0fc95881d54327edf8acfa60786fd20e
Gavin Pease <gavinpease@gmail.com>
2020-09-05 09:09:15 -0600

    Removed garbage

--- show_cal.php
+++ show_cal.php
@@ -1,15 +1,13 @@
 <?php 
-                $cookiename="loggedin";
-                      if(!isset($_COOKIE[$cookiename])) {
-header("Location: /login.php?url=".$_SERVER['REQUEST_URI']);
-                        } else {
-            
-                        }
- ?> 
+    $cookiename="loggedin";
+    if(!isset($_COOKIE[$cookiename])) {
+        header("Location: /login.php?url=".$_SERVER['REQUEST_URI']);
+    }
+?>
 <link rel="stylesheet" href="css/w3.css">
 <link href="css/style.css" rel="stylesheet" type="text/css"></link>
 <script src="js/1jquery.js"></script>
 <script src="js/sprinkler.js"></script>
 <?php 
     include_once('lib/calendar.php');
- ?>
+?>
