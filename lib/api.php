0fb023f11a12877ddb6f41b6fb39d341e5682617
gcpease <gavinpease@gmail.com>
2020-09-06 12:36:34 -0600

    Fixed array issues

--- lib/api.php
+++ lib/api.php
@@ -4,8 +4,8 @@ $sqlquery = new doSQL();
 $sqlquery->doSQLStuff( "SELECT * FROM `Systems`" );
 $gpios = $sqlquery->get_gpios();
 $id = $sqlquery->get_id();
-$array =array(array());
-for ( $i = 0; $i < sizeof( id ); $i++ ) {
+$array = array();
+for ( $i = 0; $i < sizeof( $id ); $i++ ) {
     $value = shell_exec('gpio -g read '.$gpios[$i]);
     $array[$i]->gpio = $gpios[$i];
     $array[$i]->status = $value;
