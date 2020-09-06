8576c62e620ff9ae5b8f5ec8052da5ca0307e8ff
gcpease <gavinpease@gmail.com>
2020-09-06 12:41:10 -0600

    Added ternary statement

--- lib/api.php
+++ lib/api.php
@@ -8,7 +8,7 @@ $array = array();
 for ( $i = 0; $i < sizeof( $id ); $i++ ) {
     $value = shell_exec('gpio -g read '.$gpios[$i]);
     $array[$i]->gpio = $gpios[$i];
-    $array[$i]->status = $value;
+    $array[$i]->status = ($value == 1 ? "on" : "off");
 }
 $json = json_encode($array);
 echo "system_status=".$json;
