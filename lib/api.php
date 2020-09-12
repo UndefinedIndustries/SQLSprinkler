6af03c7a8532836502d3ae5e05c1c024f729b509
gt3ch1 <gcpease@gt3ch1.tk>
2020-09-12 10:26:22 -0600

    Added &

--- lib/api.php
+++ lib/api.php
@@ -32,7 +32,7 @@ $dir=getcwd()."/";
 if ( isset( $_GET[ 'on' ] ) ) {
     $run = $_GET[ 'on' ];
     exec( "sudo ".$dir."off.py" );
-    exec( "sudo ".$dir."on.py " . $run);
+    exec( "sudo ".$dir."on.py " . $run . " & ");
     echo "Running... " . $run . " -> " .$dir;
 }
 if ( isset( $_GET[ 'off' ] ) ) {
